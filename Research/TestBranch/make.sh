323e3806307f16eb5a092c67e1a3eef6d68244f7
Henry Lovett <hl13g10@ecs.soton.ac.uk>
2014-02-06 13:05:38 +0000

    Added a primitive make file for the latex files.
    To run, be in the directory and run > ./make.sh

---
+++ Research/TestBranch/make.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+file=TestBranch
+
+#clean
+rm *.aux
+rm *.bbl
+rm *.log
+rm *.blg
+rm *.toc
+
+#make
+pdflatex ${file}.tex
+bibtex ${file}
+pdflatex ${file}.tex
+pdflatex ${file}.tex
+ 
+#clean
+rm *.aux
+rm *.bbl
+rm *.log
+rm *.blg
+rm *.toc
