fbf6503ec288f44b4bddf63d44fd740e6a365695
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-02-11 01:31:10 +0000

    #2 - Changed code for better relative stack access

--- Research/Subroutines/Code/8086Callee.asm
+++ Research/Subroutines/Code/8086Callee.asm
@@ -1,11 +1,13 @@
-adder PROC           ; Subroutine
-      PUSH  bp       ; Push base ptr to stack
-      MOV   bp,sp    ; Set base ptr to stack ptr
-      ADD   bp,4     ; Move to arg2 in stack
-      MOV   ax,[bp]  ; Load into working reg   
-      ADD   bp,2     ; Move to arg1 in stack
-      ADD   ax,[bp]  ; Add to contents of working reg 
-      MOV   [bp],ax  ; Replace arg1 with result         
-      POP   bp       ; Restore base ptr
-      RET    
-adder ENDP           
+adder PROC           ; Subroutine   
+      PUSH  bp          ; Push base ptr to stack
+      MOV   bp,sp       ; Set base ptr to stack ptr  
+      SUB   sp,4        ; Allocate local variable space (2 ints)
+      MOV   ax,[bp+4]   ; Load arg1 into Working 
+      MOV   [bp-2],ax   ; Load arg1 into Local1   
+      MOV   ax,[bp+6]   ; Load arg2 into Working     
+      ADD   ax,[bp-2]   ; Add to contents of working reg 
+      MOV   [bp-4],ax   ; Write Local2 with result 
+      MOV   sp,bp       ; Return stack ptr 
+      POP   bp          ; Restore base ptr
+      RET               ; Done
+adder ENDP                 
--- Research/Subroutines/Code/8086Caller.asm
+++ Research/Subroutines/Code/8086Caller.asm
@@ -1,9 +1,11 @@
-main:                ; Main loop  
-      MOV   ax,42    ; Load arg1
-      MOV   bx,69    ; Load arg2
-      PUSH  ax       ; Push arg1 to stack
-      PUSH  bx       ; Push arg2 to stack
-      CALL  adder    ; Call the subroutine  
-      POP   ax       ; Dummy pop from arg2 spot
-      POP   ax       ; Result pop from arg1 spot
-      JMP   main          
+main:                ; Main loop   
+      MOV   bp,sp       ; Init base ptr
+      MOV   ax,42       ; Load arg1
+      MOV   bx,69       ; Load arg2     
+      PUSH  bx          ; Push arg1 to stack
+      PUSH  ax          ; Push arg2 to stack
+      CALL  adder       ; Call the subroutine    
+      MOV   cx,[bp-12]  ; Access return value
+      POP   ax          ; Restore all registers
+      POP   bx
+      JMP   main       
