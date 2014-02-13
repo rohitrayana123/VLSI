7e8856f2c9ed01c64ddcf37e66110d5e659c797b
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-02-13 16:54:47 +0000

    cpu stim

--- Design/Implementation/verilog/cpu_core_stim.sv
+++ Design/Implementation/verilog/cpu_core_stim.sv
@@ -1,12 +1,38 @@
-module alu_stim;
+module cpu_core_stim;
 
 timeunit 1ns; timeprecision 10ps;
 
-alu alu(                                           
-   .Zflag(Zflag), 
-   .Result(Result),
-   .ACC(ACC),
-   .Mem(Mem)
+wire  [15:0]   Data_out;
+wire           ENB;     
+wire           nME;     
+wire           ALE;     
+wire           RnW;     
+wire           nOE;     
+wire           SDO;     
+logic [15:0]   Data_in; 
+logic          nIRQ;    
+logic          nWAit;   
+logic          Test;    
+logic          SDI;     
+logic          Clock;   
+logic          nReset;  
+
+
+cpu_core cpu_core(                                           
+   .Data_out   (Data_out), // Output from DUT 
+   .ENB        (ENB),
+   .nME        (nME),
+   .ALE        (ALE),
+   .RnW        (RnW),
+   .nOE        (nOE),
+   .SDO        (SDO),
+   .Data_in    (Data_in),  // Input to DUT
+   .nIRQ       (nIRQ),
+   .nWait      (nWait),
+   .Test       (Test),
+   .SDI        (SDI),
+   .Clock      (Clock),
+   .nReset     (nReset)
 );
 
 endmodule
