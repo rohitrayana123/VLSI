4671c0c95ad022937dbeb442089fe6a64aaf300b
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-02-13 15:53:23 +0000

    Add opcodes and control_stim update

--- Design/Implementation/verilog/control_stim.sv
+++ Design/Implementation/verilog/control_stim.sv
@@ -1,12 +1,47 @@
-module alu_stim;
+module control_stim;
 
 timeunit 1ns; timeprecision 10ps;
 
-alu alu(                                           
-   .Zflag(Zflag), 
-   .Result(Result),
-   .ACC(ACC),
-   .Mem(Mem)
+parameter CLK_PERIOD = 100;
+
+wire        SelInc;
+wire        LoadPC;
+wire        LoadIR;
+wire        TrisOperand;
+wire        TrisPC;
+wire        TrisAcc;
+wire        TrisMem;
+wire        ENB;
+wire        nME;
+wire        RnW;
+wire        nOE;
+logic [3:0] Opcode;
+logic       Zflag;
+logic       nWAit;
+logic       Clock;
+logic       nReset;
+
+control control(                   
+   .SelInc        (SelInc),      // Outputs from DUT
+   .LoadPC        (LoadPC),
+   .LoadIR        (LoadIR),
+   .TrisOperand   (TrisOperand),
+   .TrisPC        (TrisPC),
+   .TrisAcc       (TrisAcc),
+   .TrisMem       (TrisMem),
+   .ENB           (ENB),
+   .nME           (nME),
+   .RnW           (RnW),
+   .nOE           (nOE),
+   .Opcode        (Opcode),      // Inputs to DUT
+   .Zflag         (Zflag),
+   .nWait         (nWait),
+   .Clock         (Clock),
+   .nReset        (nReset)
 );
+
+always begin   #(CLK_PERIOD/2)   Clock = 0;
+               #(CLK_PERIOD/2)   Clock = 1;
+end
 
 endmodule
