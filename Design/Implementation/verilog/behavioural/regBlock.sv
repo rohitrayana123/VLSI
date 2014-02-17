f27015334a97c3682a9b2541b951ab2263cc7a84
Henry Lovett <hl13g10@ecs.soton.ac.uk>
2014-02-17 22:41:08 +0000

    Altered to remove zero register.
    Also added parameters to it.
    Renamed some ports to match datapath diagram

--- Design/Implementation/verilog/behavioural/regBlock.sv
+++ Design/Implementation/verilog/behavioural/regBlock.sv
@@ -1,23 +1,23 @@
-module regBlock( 
-  output logic [15:0]   Rd1,
-  output logic [15:0]   Rd2,
-  input        [15:0]   WData,
-  input        [2:0]    Rs1,
-  input        [2:0]    Rs2,
-  input        [2:0]    Rw,
+module regBlock #( parameter n = 16, parameter reg_count = 8, parameter addr_size = 3) ( 
+  output logic [n-1:0]   Rd1,
+  output logic [n-1:0]   Rd2,
+  input        [n-1:0]   WData,
+  input        [addr_size-1:0]    Rs1,
+  input        [addr_size-1:0]    Rs2,
+  input        [addr_size-1:0]    Rw,
   input                 Clock, 
-  input                 We
+  input                 WE
 );
 
 timeunit 1ns; timeprecision 100ps;
 
-logic [15:0] regs [7:1];                     // No zero register 
+logic [n-1:0] regs [reg_count-1:0];
 
-always_ff@(posedge Clock) begin
-   if(We  && (Rw != 0))  regs[Rw] <= WData;    
+always_ff@(posedge Clock) 
+begin
+   if(WE)  regs[Rw] <= WData;    
 end
 
-assign Rd1 = (Rs1 == 0) ? 0 : regs[Rs1];     // Deal with zero addressing
-assign Rd2 = (Rs2 == 0) ? 0 : regs[Rs2];
-
+assign Rd1 = regs[Rs1];
+assign Rd2 = regs[Rs2];
 endmodule
