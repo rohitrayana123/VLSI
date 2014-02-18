e569c4a170e3a1ffc20946e3bac87752d3a8fb5a
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-02-18 01:44:12 +0000

    More report pics

--- Research/Subroutines/Code/ArmCallee.asm
+++ Research/Subroutines/Code/ArmCallee.asm
@@ -1,8 +1,8 @@
-adder PUSH  {r5-r7,lr}     ; Link register holds return address
+adder PUSH  lr             ; Link register holds return address
       SUB   sp,sp,#4       ; Make space for locals on the stack
       LDR   r0, [sp,#12]   ; Get arg1 off stack
       LDR   r1, [sp,#8]    ; Get arg2 off stack
-      ADD   r0,r1          ; Do the add
+      ADD   r0,r0,r1       ; Do the add
       STR   r0,[sp,#0]     ; Store the add as a local
       ADD   sp,sp,#4       ; Destroy local variable space
-      POP   {r5-r7,pc}     ; Restore program counter and return 
+      POP   pc             ; Restore program counter and return 
