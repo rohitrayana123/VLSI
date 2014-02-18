4515d79b73627fc4f9e7eb7ac7d1ecf8298211c6
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-02-18 15:00:11 +0000

    Control stuff

--- Design/Implementation/verilog/behavioural/control.sv
+++ Design/Implementation/verilog/behavioural/control.sv
@@ -1,4 +1,6 @@
 module control(
+   output logic            Ale,
+   output logic            Enb,
    output logic   [4:0]    AluOp, 
    output logic   [1:0]    Op2Sel, 
    output logic            Op1Sel, 
@@ -11,18 +13,97 @@ module control(
    output logic            PcWe,
    output logic   [1:0]    PcSel,
    output logic            PcEn,
-   output logic            IrEn,
    output logic            IrWe,
    output logic            WdSel,
    output logic            ImmSel,
    output logic            RegWe,
    output logic            MemEn,
-   input  wire    [7:0]    IrControl,
+   input  wire    [7:0]    OpCode,
    input  wire             Z,
    input  wire             Clock,
    input  wire             nReset
 );
 
 timeunit 1ns; timeprecision 100ps;
+
+enum {
+   fetch,
+   decode,
+   execute
+}  state;
+
+enum {
+   addrLatch,
+   irGet       
+}  fetchSub;
+enum {
+   doing,
+   done        
+}  executeSub;
+
+always_ff@(posedge Clock or negedge nReset) begin
+   // Major states
+   if(!nReset) 
+      state <= fetch; 
+   else        
+      case(state) 
+         fetch    :  if(fetchSub == irGet)
+                        state <= decode;
+         decode   :  state <= execute;
+         execute  :  if(executeSub == done)
+                        state <= fetch;
+         default  :  state <= fetch;
+      endcase
+   // Fetch
+   if(!nReset) 
+      fetchSub <= addrLatch;
+   else     
+      if(state == fetch)
+         case(fetchSub)
+            addrLatch   : fetchSub <= irGet;
+            irGet       : fetchSub <= addrLatch;
+            default     : fetchSub <= addrLatch;
+         endcase
+   // Execute
+   if(!nReset)
+      executeSub <= doing;
+   else
+      if(state == execute)
+         case(executeSub)
+            doing       : executeSub <= done;
+            done        : executeSub <= doing;
+         endcase
+end
+
+always_comb begin
+   Ale   <= 0;
+   Enb   <= 0;
+   PcEn  <= 0;
+   IrWe  <= 0;
+   RegWe <= 0;
+   case(state) 
+      fetch    :
+         case(fetchSub)
+            addrLatch   : begin
+               Ale   <= 1;
+               Enb   <= 1;
+               PcEn  <= 1;
+            end
+            irGet       : begin
+               IrWe  <= 1; 
+            end
+         endcase
+      execute  :
+         case(OpCode)
+            0 : begin
+               Op1Sel <= 1;
+               Op2Sel <= 1;
+               WdSel  <= 1;
+               AluOp  <= 5'h1F;
+            end 
+         endcase
+   endcase
+end
+
 
 endmodule
--- Design/Implementation/verilog/behavioural/control_stim.sv
+++ Design/Implementation/verilog/behavioural/control_stim.sv
@@ -4,44 +4,62 @@ timeunit 1ns; timeprecision 10ps;
 
 parameter CLK_PERIOD = 100;
 
-wire        SelInc;
-wire        LoadPC;
-wire        LoadIR;
-wire        TrisOperand;
-wire        TrisPC;
-wire        TrisAcc;
-wire        TrisMem;
-wire        ENB;
-wire        nME;
-wire        RnW;
-wire        nOE;
-logic [3:0] Opcode;
-logic       Zflag;
-logic       nWAit;
-logic       Clock;
-logic       nReset;
+logic       Ale;
+logic       Enb;
+logic [4:0] AluOp;    
+logic [1:0] Op2Sel;   
+logic       Op1Sel;   
+logic       Rw;       
+logic       AluEn;    
+logic       SpEn;     
+logic       SpWe;     
+logic       LrEn;     
+logic       LrWe;     
+logic       PcWe;     
+logic [1:0] PcSel;    
+logic       PcEn;     
+logic       IrWe;     
+logic       WdSel;    
+logic       ImmSel;   
+logic       RegWe;    
+logic       MemEn;    
+logic [7:0] OpCode;
+logic       Z;        
+logic       Clock;    
+logic       nReset;   
 
-control control(                   
-   .SelInc        (SelInc),      // Outputs from DUT
-   .LoadPC        (LoadPC),
-   .LoadIR        (LoadIR),
-   .TrisOperand   (TrisOperand),
-   .TrisPC        (TrisPC),
-   .TrisAcc       (TrisAcc),
-   .TrisMem       (TrisMem),
-   .ENB           (ENB),
-   .nME           (nME),
-   .RnW           (RnW),
-   .nOE           (nOE),
-   .Opcode        (Opcode),      // Inputs to DUT
-   .Zflag         (Zflag),
-   .nWait         (nWait),
-   .Clock         (Clock),
-   .nReset        (nReset)
+control control(
+   .Ale           (Ale        ),
+   .Enb           (Enb        ),
+   .AluOp         (AluOp      ),            
+   .Op2Sel        (Op2Sel     ),
+   .Op1Sel        (Op1Sel     ),
+   .Rw            (Rw         ),
+   .AluEn         (AluEn      ),      
+   .SpEn          (SpEn       ), 
+   .SpWe          (SpWe       ),
+   .LrEn          (LrEn       ),
+   .LrWe          (LrWe       ),
+   .PcWe          (PcWe       ),
+   .PcSel         (PcSel      ),
+   .PcEn          (PcEn       ),
+   .IrWe          (IrWe       ),
+   .WdSel         (WdSel      ),
+   .ImmSel        (ImmSel     ),
+   .RegWe         (RegWe      ),
+   .MemEn         (MemEn      ),
+   .OpCode        (OpCode     ),
+   .Z             (Z          ),
+   .Clock         (Clock      ),
+   .nReset        (nReset     )
 );
 
 always begin   #(CLK_PERIOD/2)   Clock = 0;
                #(CLK_PERIOD/2)   Clock = 1;
 end
 
+initial begin
+         nReset = 0;
+   #500  nReset = 1;
+end
 endmodule
