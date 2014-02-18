9c5f49088ae17f2a7f09ae0ae9f9ef96688e66b9
Henry Lovett <hl13g10@ecs.soton.ac.uk>
2014-02-18 13:53:06 +0000

    Now uses the trisreg module.
    Done most the internal wiring

--- Design/Implementation/verilog/behavioural/datapath.sv
+++ Design/Implementation/verilog/behavioural/datapath.sv
@@ -1,47 +1,19 @@
 module datapath(
   output wire  [15:0]   SysBus,
   output wire  [7:0]    Opcode,
-  output logic          Z,
+  output logic [3:0]    Flags,
   input  wire  [15:0]   DataIn,
   input  wire  [4:0]    AluOp,
-  input  wire  [1:0]    Op2Sel,
-  input  wire           Op1Sel,
-  input  wire           Rw,
-  input  wire           AluEn,
-  input  wire           SpEn,
-  input  wire           SpWe,
-  input  wire           LrEn,
-  input  wire           LrWe,
-  input  wire           PcWe,
-  input  wire  [1:0]    PcSel,
-  input  wire           PcEn,
-//input  wire           IrEn,
-  input  wire           IrWe,
-  input  wire           WdSel,
-  input  wire           ImmSel,
-  input  wire           RegWe,
-  input  wire           MemEn,
-  input  wire           Clock,
-  input  wire           nReset
+  input  wire  [1:0]    Op2Sel, PcSel,
+  input  wire           Op1Sel, Rw, AluEn, SpEn, SpWe, LrEn, LrWe, PcWe, PcEn, IrWe, WdSel, ImmSel, RegWe, MemEn, Clock, nReset
 );
 
 timeunit 1ns; timeprecision 100ps;
 
-wire  [15:0]   AluRes;
-wire  [15:0]   AluBusIF;
-logic [15:0]   Op1;
-logic [15:0]   Op2;
-wire  [15:0]   Rd1;
-wire  [15:0]   Rd2;
-wire  [15:0]   WData;
-wire  [15:0]   SignExtend;
-logic [15:0]   AluOut;
-logic [15:0]   Pc;
-wire  [15:0]   PcSrc;
-logic [15:0]   Sp;
-logic [15:0]   Lr;
-logic [15:0]   Ir;
-wire  [15:0]   Extended;
+wire  [15:0]   AluRes, AluBusIF, Rd1, Rd2, WData, SignExtend, Extended, PcSrc;
+logic [15:0]   Op1, Op2, AluOut, Pc, PcIn, Sp, Lr, Ir;
+
+
 
 // Instances
 
@@ -56,42 +28,90 @@ regBlock regBlock(      // Register block instance
    .nReset  (nReset  ),
    .We      (RegWe   )
 );
-alu alu(                // Combo ALU only
-   .Zflag   (Zflag   ), 
+alu a(                // Combo ALU only
+   .Flags   (Flags   ), 
    .Result  (AluRes  ),
    .Op1     (Op1     ),
    .Op2     (Op2     ),
    .OpCode  (AluOp   )
 );
-//Sign extends
 assign Extended = (ImmSel) ? {{11{Ir[10]}}, Ir[10:6] } : { {8{Ir[10]}}, Ir[10:6]};
-
+assign Opcode = {Ir[15:9], Ir[2:0]};
+assign Sysbus = (MemEn) ? DataIn : {16{1'bz}};
 // Bus interfaces
 
-trisBuf16 trisBufAlu(   // ALU
-   .DataIn  (AluOut  ),
-   .En      (AluEn   ),
-   .DataOut (SysBus  )
+//trisBuf16 trisBufAlu(   // ALU
+//   .DataIn  (AluOut  ),
+//   .En      (AluEn   ),
+//   .DataOut (SysBus  )
+//);
+//trisBuf16 trisBufPc(    // PC
+//   .DataIn  (Pc      ),
+//   .En      (PcEn    ),
+//   .DataOut (SysBus  )
+//);
+//trisBuf16 trisBufSp(    // SP
+//   .DataIn  (Sp      ),
+//   .En      (SpEn    ),
+//   .DataOut (SysBus  )
+//);
+//trisBuf16 trisBufLr(    // LR
+//   .DataIn  (Lr      ),
+//   .En      (LrEn    ),
+//   .DataOut (SysBus  )
+//);
+//trisBuf16 trisBufSys(   // Memory access
+//   .DataIn  (DataIn  ),
+//   .En      (MemEn    ),
+//   .DataOut (SysBus  )
+//);
+ 
+
+//Registers
+trisreg Reg_PC (
+	.Clock  (Clock   ), 
+	.nReset (nReset  ),
+	.Reg_EN (PcEn    ),
+	.Reg_WE (PcWe    ),
+	.DataIn (PcIn    ),
+	.DataOut(Pc      ),
+	.TrisOut(SysBus  )
 );
-trisBuf16 trisBufPc(    // PC
-   .DataIn  (Pc      ),
-   .En      (PcEn    ),
-   .DataOut (SysBus  )
+trisreg Reg_LR (
+	.Clock  (Clock   ), 
+	.nReset (nReset  ),
+	.Reg_EN (LrEn    ),
+	.Reg_WE (LrWe    ),
+	.DataIn (        ),
+	.DataOut(        ),
+	.TrisOut(SysBus  )
 );
-trisBuf16 trisBufSp(    // SP
-   .DataIn  (Sp      ),
-   .En      (SpEn    ),
-   .DataOut (SysBus  )
+trisreg Reg_SP (
+	.Clock  (Clock   ), 
+	.nReset (nReset  ),
+	.Reg_EN (SpEn    ),
+	.Reg_WE (SpWe    ),
+	.DataIn (AluOut  ),
+	.DataOut(Sp      ),
+	.TrisOut(SysBus  )
 );
-trisBuf16 trisBufLr(    // LR
-   .DataIn  (Lr      ),
-   .En      (LrEn    ),
-   .DataOut (SysBus  )
+trisreg Reg_IR (
+	.Clock  (Clock   ), 
+	.nReset (nReset  ),
+	.Reg_EN (0       ),//does not utilise the tristate bus as an output
+	.Reg_WE (IrWe    ),
+	.DataIn (SysBus  ),
+	.DataOut(Ir      ),
+	.TrisOut(SysBus  )
 );
-trisBuf16 trisBufSys(   // Memory access
-   .DataIn  (DataIn  ),
-   .En      (MemEn    ),
-   .DataOut (SysBus  )
+trisreg Reg_ALUOUT (
+	.Clock  (Clock   ), 
+	.nReset (nReset  ),
+	.Reg_EN (AluEn   ),
+	.Reg_WE (AluWe   ),
+	.DataIn (AluRes  ),
+	.DataOut(AluOut  ),
+	.TrisOut(SysBus  )
 );
 
 
@@ -99,38 +119,35 @@ trisBuf16 trisBufSys(   // Memory access
 
 // Sequential
 
-always_ff@(posedge Clock or negedge nReset) begin : AluReg
-   if(!nReset)
-      AluOut <= 0;
-   else
-      AluOut<= AluRes;
-end
-always_ff@(posedge Clock or negedge nReset) begin : SpReg
-   if(!nReset)
-      Sp <= 0;
-   else
-      if(SpWe)
-         Sp <= AluOut;
-end
-always_ff@(posedge Clock or negedge nReset) begin : PcRegMuxed 
-   if(!nReset)      
-      Pc <= 0;
-   else
-      if(PcWe)                            // Control PC data input
-         case(PcSel)                      // 3 input mux
-            0        :  Pc <= Lr;
-            1        :  Pc <= AluOut;
-            2        :  Pc <= SysBus;
-            default  :  Pc <= Pc + 1;
+//always_ff@(posedge Clock or negedge nReset) begin : AluReg
+//   if(!nReset)
+//      AluOut <= 0;
+//   else
+//      AluOut<= AluRes;
+//end
+//always_ff@(posedge Clock or negedge nReset) begin : SpReg
+//   if(!nReset)
+//      Sp <= 0;
+//   else
+//      if(SpWe)
+//         Sp <= AluOut;
+//end
+always_comb //@(posedge Clock or negedge nReset) begin : PcRegMuxed 
+begin : PcInMux
+	case(PcSel)                      // 3 input mux
+            0        :  PcIn <= Lr;
+            1        :  PcIn <= AluOut;
+            2        :  PcIn <= SysBus;
+            default  :  PcIn <= Pc + 1;
          endcase
 end
-always_ff@(posedge Clock or negedge nReset) begin : IrReg
-   if(!nReset)
-      Ir <= 0;
-   else
-      if(IrWe)
-         Ir <= SysBus;
-end
+//always_ff@(posedge Clock or negedge nReset) begin : IrReg
+//   if(!nReset)
+//      Ir <= 0;
+//   else
+//      if(IrWe)
+//         Ir <= SysBus;
+//end
 assign IrControl = {Ir[15:9],Ir[2:0]};                 // Wire these to OpCode output
 
 // Combo
