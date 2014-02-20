192be1a1bfc33f93ebe8cfe37e812627bf71e556
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-02-20 17:38:26 +0000

    Moved handin script 2 bin

---
+++ bin/SubmitBehavioural.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Sort out folder to copy to
+HOME="home"
+DESIGN="design"
+FCDE="fcde"
+while true; do
+   read -p "Remove everything in /$HOME/$USER/$DESIGN/$FCDE ?  "  yn
+   case $yn in
+      [Yy]* ) rm -rf /$HOME/$USER/$DESIGN/$FCDE; break;;
+      [Nn]* ) echo "Exiting"; exit;;
+      * ) echo "Please answer yes or no.";;
+   esac
+done
+mkdir /$HOME/$USER/$DESIGN/$FCDE
+# Folder layout
+DES="../Design"
+IMP="Implementation"
+# Copy the verilog folder
+VER="verilog"
+echo "Copying across $VER folder..."
+mkdir /$HOME/$USER/$DESIGN/$FCDE/$VER
+cp -rf $DES/$IMP/$VER/*  /$HOME/$USER/$DESIGN/$FCDE/$VER
+ls -l /$HOME/$USER/$DESIGN/$FCDE/$VER 
+echo ""
+#Copy the verify folder
+VERIF="verification"
+BER="behavioural"
+echo "Copying across $VERIF folder..."
+cp -rf $DES/$IMP/$VERIF/*  /$HOME/$USER/$DESIGN/$FCDE/$VER/$BER
+ls -l /$HOME/$USER/$DESIGN/$FCDE/$VER 
+echo ""
+
+
+
+
+#################################
+# TODO: Make sure this copies everything needed for BIM handin script
+################################
+
+
+
+
+
+
+
+# Call the handin script
+HANDIN="prepare_fcde behavioural"
+cd /$HOME/$USER/$DESIGN/$FCDE
+$HANDIN
