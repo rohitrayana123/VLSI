cae5d63057f72158b966c34f6f5c91954f08899a
seblovett <seblovett@googlemail.com>
2014-02-21 09:37:04 +0000

    Removed lots of temp files.
    Also, added them to the .gitignore so they don't crop up again

--- Examples/SimpleCISC/INCA_libs/irun.lnx86.10.20.nc/files.ts
+++
@@ -1,5 +0,0 @@
-1391096105 /home/mw20g10/VLSI/GIT/Examples/SimpleCISC/library/opcodes.svh
-1391096105 /home/mw20g10/VLSI/GIT/Examples/SimpleCISC/system.sv
-1391096105 /home/mw20g10/VLSI/GIT/Examples/SimpleCISC/programs/multiply.sv
-1392295726 /home/mw20g10/VLSI/GIT/Examples/SimpleCISC/programs/newcode.sv
-1391096105 /home/mw20g10/VLSI/GIT/Examples/SimpleCISC/programs/array_mult.sv
