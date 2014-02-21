4796f8afc4407c5e8832f01537e86f72f06f3f78
seblovett <seblovett@googlemail.com>
2014-02-21 10:04:17 +0000

    Added the rest of the opcodes and the branch codes

--- Design/Implementation/verilog/behavioural/opcodes.svh
+++ Design/Implementation/verilog/behavioural/opcodes.svh
@@ -9,33 +9,49 @@ typedef
 //Program Counter Selction Codes
 typedef enum logic [1:0] {PcLr, PcSysbus, PcAluOut, Pc1 } pc_select_t;
 typedef enum logic [1:0] {Op1Pc, Op1Rd1, Op1Sp } Op1_select_t;
+typedef enum logic [2:0] {	BR  = 3'b000, 
+				BNE = 3'b110, 
+				BE  = 3'b111, 
+				BLT = 3'b100, 
+				BGE = 3'b101, 
+				BWL = 3'b011, 
+				RET = 3'b010, 
+				JMP = 3'b001 
+				} Branch_t;
 // Define Opcodes:
 //
 //  these could be defined with a further enumerated type declaration
 //  unfortunately this doesn't easily map to a situation where opcode lengths are variable
 //
-//localparam NOP		= 4'd0;
-//localparam JMP		= 4'd1;
-//localparam JMPZ		= 4'd2;
-//localparam JMPNZ	= 4'd3;
-//localparam LDA		= 4'd4;
-//localparam ADD		= 4'd5;
-//localparam SUB		= 4'd6;
-//localparam AND		= 4'd7;
-//localparam OR		= 4'd8;
-//localparam NOT		= 4'd9;
-//localparam LSL		= 4'd10;
-//localparam LSR		= 4'd11;
-//localparam STA		= 4'd15;
 
-localparam NOP 			= 5'b00000;
-localparam ADD 			= 5'b00100;
-localparam ADDI 		= 5'b00101;
-localparam ADDIB	 	= 5'b11000;
-localparam ADC			= 5'b00110;
-localparam ADCI			= 5'b00111;
-
-
-
+localparam NOP 			= 5'b00000; //Type A
+localparam ADD 			= 5'b00100; //Type A
+localparam ADDI 		= 5'b00101; //Type A
+localparam ADDIB	 	= 5'b11000; //Type B
+localparam ADC			= 5'b00110; //Type A
+localparam ADCI			= 5'b00111; //Type A
+localparam NEG                  = 5'b01000; //Type A
+localparam SUB                  = 5'b01001; //Type A
+localparam SUBI                 = 5'b01010; //Type A
+localparam SUBIB                = 5'b11001; //Type B
+localparam SUC                  = 5'b01011; //Type A
+localparam SUCI                 = 5'b01100; //Type A
+localparam CMP                  = 5'b01101; //Type A
+localparam AND                  = 5'b10000; //Type A
+localparam OR                   = 5'b10001; //Type A
+localparam XOR                  = 5'b10010; //Type A
+localparam NOT                  = 5'b10011; //Type A
+localparam NAND                 = 5'b10100; //Type A
+localparam NOR                  = 5'b10101; //Type A
+localparam LSL                  = 5'b00001; //Type A
+localparam LSR                  = 5'b00010; //Type A
+localparam ASR                  = 5'b00011; //Type A
+localparam LDW                  = 5'b11101; //Type C
+localparam STW                  = 5'b11101; //Type C
+localparam LUI                  = 5'b11010; //Type B
+localparam LLI                  = 5'b11011; //Type B
+localparam BRANCH               = 5'b11111; //Type D
+localparam PUSH                 = 5'b11100; //Type E
+localparam POP                  = 5'b11100; //Type E
 
 endpackage
