29d14cb7493a488a2835ffb75b2c9c2a964499fc
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-02-24 12:22:21 +0000

    #32 - Some rectified

--- Design/Implementation/verilog/behavioural/datapath.sv
+++ Design/Implementation/verilog/behavioural/datapath.sv
@@ -17,13 +17,13 @@ logic [15:0]   Op1, Op2, AluOut, Pc, PcIn, Sp, Lr, Ir, LrIn;
 logic [2:0] Rs1In;
 
 //Combinational logic for tristate bus, reg inputs or outputs
-assign Extended = (ImmSel) ? {{11{Ir[10]}}, Ir[10:6] } : { {8{Ir[10]}}, Ir[10:6]};
+assign Extended = (ImmSel) ? {{11{Ir[4]}}, Ir[4:0] } : { {8{Ir[7]}}, Ir[7:0]};
 assign Opcode = {Ir[15:9], Ir[2:0]};
 assign SysBus = (MemEn) ? DataIn : {16{1'bz}};
 assign WData = (WdSel) ? SysBus : AluRes; // 2 input mux
 assign Op2 = (Op2Sel) ? Rd2 : Extended;
 assign LrIn = (LrSel) ? Pc : SysBus;
-assign Rs1In = (Rs1Sel) ? Ir[2:0] : Ir[5:3]; 
+assign Rs1In = (Rs1Sel) ? Ir[4:2] : Ir[10:8]; 
 //Multiplexers
 always_comb begin : PcInMux
 	case(PcSel)                      // 3 input mux
@@ -54,8 +54,8 @@ regBlock regBlock(      // Register block instance
    .Rd2     (Rd2     ),
    .WData   (WData   ),
    .Rs1     (Rs1In   ),
-   .Rs2     (Ir[8:6] ),
-   .Rw      (Ir[2:0] ),
+   .Rs2     (Ir[7:5] ),
+   .Rw      (Ir[10:8]),
    .Clock   (Clock   ),
    .nReset  (nReset  ),
    .We      (RegWe   )
