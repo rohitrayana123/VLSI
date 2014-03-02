6f408527cc29f17c8fa0e14d5ab56a50e6080d6b
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-03-02 22:53:17 +0000

    Working multilp prog (no too spec)

--- Design/Implementation/programs/multiply.asm
+++ Design/Implementation/programs/multiply.asm
@@ -1,19 +1,25 @@
-		BR .main
-.multi 	LDW R0,[R7,2]
-		LDW R1,[R7,3]
-		ADD R0,R1,R1
-		STW R0,[R7,1]	; Store in place holder
-		RET
-.main 	SUB R0,R0,R0	; Init regs
+		SUB R0,R0,R0	; Init regs
 		SUB R1,R1,R1
 		SUB R2,R2,R2
 		SUB R3,R3,R3
 		SUB R4,R4,R4
 		SUB R5,R5,R5
 		SUB R6,R6,R6
-		SUB R7,R7,R7	; Setup SP
-		LUI R7, #7
-		LLI R7, #255	; Put stuff here...
+		SUB R7,R7,R7	
+		LUI R7, #7		; Setup SP
+		BR .main
+.multi  LDW R0,[R7,2]	; Op1 in R0
+		LDW R1,[R7,3]	; Op2 in R1
+		SUB R2,R2,R2	; Zero in R2
+        ADDIB R1,#0		; Check Op2 is not zero
+		BNE .done
+.loop  ADD R2,R2,R0	; Add Op1
+		SUBIB R1,#1		; Dec Op2
+		BE .loop
+.done  STW R2,[R7,1]	; Store result in place holder
+		RET
+.end  BR .end			; finshed loop
+.main  LLI R7, #255	
 		LUI R0, #8		; Address in R0
 		LLI R0, #0
 		LDW R1,[R0,0]	; Switches in R1
@@ -40,8 +46,4 @@
 		LUI R1, #8
 		LLI R1, #1		; Addres of LEDS
 		STW R0,[R1,0]	; Result on LEDS
-		LUI R0, #255	; Write to stop mem pos - Not for BIM
-		LLI R0, #255
-		LUI R1, #4 
-		LLI R1, #0	
-		STW R0,[R1,0]
+		BR .end         ; done	
