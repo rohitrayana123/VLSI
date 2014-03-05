ecfee480f5c04b57184466b3ebdf1f91a3c263f8
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-03-05 15:28:19 +0000

    Data sink

--- Design/Implementation/programs/interrupt.asm
+++ Design/Implementation/programs/interrupt.asm
@@ -1,30 +1,31 @@
 		LUI R7, #7		; Setup SP
 		LLI R7, #255
-		LUI	R0,	#1		; R0 is read ptr
-		LUI R1, #1		' R1 in write ptr
-		LLI R1, #1
-		TOGI			; Enable interrupts
-		LUI R0,#A0		; Address of Serial control reg
+		LUI	R0,	#1		; R0 is read ptr	0x0100
+		ADDI R1,R0,#1	; 0x0101
+		STW R1,[R0,#0]	; Read ptr set to   0x0101		
+		STW R1,[R0,#1]	; Write ptr set to 	0x0101
+		ADDIB R0,#0		; ENINT			; Enable interrupts
+		LUI R0,#160		; Address of Serial control reg
 		LLI R1,#01		; Data to enable ints
 		STW R1,[R0,#1]	; Store 0x001 @ 0xA001		AJR - Ints from here should be OK
-		LLI R0,#17		; main line -1 in R0	
-		JMP R0,#0		; Jump to main, line number - 1
+		LLI R3,#18		; main line -1 in .main
+		BR .main		;JMP R3,#0		; Jump to main, line number - 1
 		ADDIB R0,#0		; Unreachable code to position isr
 		ADDIB R0,#0
 		ADDIB R0,#0
 		ADDIB R0,#0
-		ADDIB R0,#0
-		ADDIB R0,#0
-		ADDIB R0,#0
-		ADDIB R0,#0	
 .isr  	ADDIB R0,#0
-		RETI
-.main	SUB R0,R0,R0	; START OF MAIN
-		SUB R1,R1,R1
-		SUB R2,R2,R2
-		SUB R3,R3,R3	; Dummy prog, waiting for int
-		SUB R4,R4,R4
-		BWL .isr
-		SUB R5,R5,R5
-		SUB R6,R6,R6
-		BR .main       	; done	
+		RET
+.main	LUI R0, #1		; Read ptr address in R0
+		LLI R0, #0	
+		ADDI R1,R0,#1	; No data position in R1
+		LDW R2,[R0,#0]	; Read ptr in R2
+		SUB R0,R1,R2	 
+		BNE .main		; Jump back if in default position
+		LDW R3,[R2,#0] 	; Load data out of buffer
+		LUI R4,#8	
+		LLI R4,#1		; Address of LEDs
+		STW R3,[R4,#0]	; Put read data on LEDs
+		SUBIB R2,#1		; Dec read ptr
+		STW R2,[R0,#0]	; Store new read pointer
+		BR .main       	; look again	
