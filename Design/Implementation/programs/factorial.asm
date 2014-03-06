341e41d5b563293fd8e7f6e937c35aaa1cbdb850
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-03-06 20:41:24 +0000

    RET in R0 bug?

--- Design/Implementation/programs/factorial.asm
+++ Design/Implementation/programs/factorial.asm
@@ -1,14 +1,31 @@
+; Compute the factorial of R0
+
 		; Sys Init		
-		LUI R7, #7		; Setup SP
-		LLI R7, #255
+		LUI SP, #7		; Setup SP
+		LLI SP, #255
 		
 		; main
+		LLI R6,5
+		SUBIB SP,#1
+		STW R6,[SP,#0]
 		BWL .fact		; Run Subroutine
-	
+		LDW R6,[SP,#0]
+		ADDIB SP,#1
+		
 		; Finish loop
 .end 	BR .end
 
 		; Recurrisve factorial
-.fact  	PUSH Lr
-		POP Lr
+.fact  	SUBIB SP,#1
+		PUSH Lr
+		LDW R0,[SP,#1]			
+		SUBIB R0,#1
+		BNE .zero
+		SUBIB SP,#1
+		STW R0,[SP,#0]
+		BWL .fact
+		LDW R0,[SP,#0]
+		ADDIB SP,#1
+.zero   POP Lr
+		ADDIB SP,#1
 		RET
