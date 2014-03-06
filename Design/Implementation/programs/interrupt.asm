6d9529cb9f28423444e7dda6e40c32a8137eb822
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-03-06 15:15:45 +0000

    Better buffering, not finished yet

--- Design/Implementation/programs/interrupt.asm
+++ Design/Implementation/programs/interrupt.asm
@@ -1,9 +1,9 @@
 		LUI R7, #7		; Setup SP
 		LLI R7, #255
 		LUI	R0,	#1		; R0 is read ptr	0x0100
-		ADDI R1,R0,#1	; 0x0101
-		STW R1,[R0,#0]	; Read ptr set to   0x0101		
-		STW R1,[R0,#1]	; Write ptr set to 	0x0101
+		ADDI R1,R0,#2	; 0x0102
+		STW R1,[R0,#0]	; Read ptr set to   0x0102			
+		STW R1,[R0,#1]	; Write ptr set to 	0x0102
 		ENAI			; Enable interrupts
 		LUI R0,#160		; Address of Serial control reg
 		LLI R1,#01		; Data to enable ints
@@ -32,24 +32,22 @@
 		; Read serial data
 		LUI R0,#160
 		LLI R0,#0
-		STW R0,[R0,#0]	; R0 contains read serial data
-	
-		; Get write ptr
-		LUI R1,#1
-		LLI R1,#1
-		LDW R2,[R1,#0]	; R2 contain the write ptr
-	
-		; Test write ptr
-		LUI R3,#1
-		LLI R3,#5
-		SUB R3,R3,R1	
-		BNE .skip		; Buffer full, don't write
+		LDW R1,[R0,#0]	; R1 contains read serial data
 
+		; Get read ptr
+		LUI R0,#1
+		LLI R0,#0
+		LDW R2,[R0,#0]	; R2 contains read ptr
+		
+		; Get write ptr
+		ADDI R3,R0,#1
+		LDW R4,[R3,#0]	; R4 contain the write ptr
+	
 		; Put serial data at ptr address
-		STW R0,[R2,#0]	; Add to buffer
-		ADDIB R2,#1		; inc write ptr
-		STW R2,[R1,#0]
-				
+		STW R1,[R4,#0]	; Write to buffer
+		ADDIB R4,#1
+		STW R4,[R3,#0]	; Inc write ptr
+						
 .skip	LDW R6,[R7,#0]	; Pop data	; Rstore regs
 		ADDIB R7,#1		; Iin SP
 		LDW R5,[R7,#0]	; Pop data
@@ -68,14 +66,14 @@
 		
 .main	LUI R0, #1		; Read ptr address in R0
 		LLI R0, #0	
-		ADDI R1,R0,#1	; No data position in R1
 		LDW R2,[R0,#0]	; Read ptr in R2
-		SUB R0,R1,R2	 
-		BNE .main		; Jump back if in default position
+		LDW R3,[R0,#1]	; Write ptr in R3
+		SUB R5,R2,R3		 
+		BNE .main		; Jump back if the same
 		LDW R3,[R2,#0] 	; Load data out of buffer
 		LUI R4,#8	
 		LLI R4,#1		; Address of LEDs
 		STW R3,[R4,#0]	; Put read data on LEDs
-		SUBIB R2,#1		; Dec read ptr
+		ADDIB R2,#1		; Inc read ptr
 		STW R2,[R0,#0]	; Store new read pointer
 		BR .main       	; look again	
