857d0f24bb44d35049abfe52b53e4a6fc381976a
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-03-08 18:18:39 +0000

    Optimised

--- Design/Implementation/programs/factorial.asm
+++ Design/Implementation/programs/factorial.asm
@@ -1,67 +1,51 @@
-; Compute the factorial of R0
-
 		; Sys Init		
 		LUI SP, #7		; Setup SP
-		LLI SP, #255
-
-
+		LLI SP, #208
 		
-		LLI R6,#2
-		; zero test
-		PUSH R6			; Pass para
-		PUSH R6			; placeholder for result
-		BWL .multi		; Run Subroutine
-		POP R6
-		POP R6
-
 		; main
-		LLI R5,#6	
+		LLI R5,#8	
 		PUSH R5			; Pass para
-		PUSH R0			; placeholder for result
 		BWL .fact		; Run Subroutine
-		POP R0
-		POP R5
-		
+		POP R5			; Para overwritten with result	
+
 		; Finish loop
 .end 	BR .end
 
 		; Recurrisve factorial
 .fact  	PUSH R0
 		PUSH R1
-		PUSH R2
 		PUSH LR		
-		LDW R1,[SP,#5]	; Get para				
+		LDW R1,[SP,#3]	; Get para				
 		ADDIB R1,#0
 		BNE .retOne		; 0! = 1
 		SUBI R0,R1,#1
 		PUSH R0			; Pass para
-		PUSH R0			; placeholder
-		BWL .fact
-		POP R0
-		POP R2
-		PUSH R2			; Pass para
+		BWL .fact		; The output from fact to multi remains on the stack		
 		PUSH R1			; Pass para
-		BWL .multi
+		BWL .multi		
 		POP R1			; Get res
-		POP R0
-		BR .done	
-.retOne	ADDIB R1,#1
-.done   STW R1,[SP,#5]
+		ADDIB SP,#1		; POP
+		STW R1,[SP,#3]
 		POP LR
-		POP R2
+		POP R1
+		POP R0
+		RET
+.retOne	ADDIB R1,#1		; Trade off code size to avoid jump checking	
+		STW R1,[SP,#3]
+		POP LR
 		POP R1
 		POP R0
 		RET
 
 
 		; Multiply function
-.multi	LDW R0,[SP,#0]
-		LDW R1,[SP,#1]
-		SUB R2,R2,R2
-		ADDIB R1,#0
+.multi	LDW R2,[SP,#0]	; Use different regs to avoid context save
+		LDW R3,[SP,#1]
+		SUB R4,R4,R4
+		ADDIB R3,#0
 		BNE .done
-.loop	ADD R2,R2,R0
-		SUBIB R1,#1
+.loop	ADD R4,R4,R2
+		SUBIB R3,#1
 		BE .loop
-.done 	STW R2,[SP,#0]
+.done 	STW R4,[SP,#0]
 		RET
