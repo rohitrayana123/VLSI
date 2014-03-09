e73441f2f93b1bc09c555e3ff5c756ffe2e50aed
Ashley J Robinson <ajr2g10@ecs.soton.ac.uk>
2014-03-09 16:48:35 +0000

    Better buffer

--- Design/Implementation/programs/interrupt.asm
+++ Design/Implementation/programs/interrupt.asm
@@ -1,49 +1,65 @@
+		DISI			; Reset is off anyway
 		LUI R7, #7		; Setup SP
 		LLI R7, #255
 		LUI	R0,	#1		; R0 is read ptr	0x0100
 		ADDI R1,R0,#2	; 0x0102
 		STW R1,[R0,#0]	; Read ptr set to   0x0102			
 		STW R1,[R0,#1]	; Write ptr set to 	0x0102
-		ENAI			; Enable interrupts
 		LUI R0,#160		; Address of Serial control reg
 		LLI R1,#01		; Data to enable ints
-		STW R1,[R0,#1]	; Store 0x001 @ 0xA001		AJR - Ints from here should be OK
+		STW R1,[R0,#1]	; Store 0x001 @ 0xA001
 		LLI R3,#18		; main line -1 in .main
-		BR .main		;JMP R3,#0		; Jump to main, line number - 1	
+		ENAI
+		BR .main
 		ADDIB R0,#0
 		ADDIB R0,#0
 		ADDIB R0,#0
-		ADDIB R0,#0
-.isr  	PUSH R0
+.isr  	DISI			; Don's lose event
+		PUSH R0			; Save only this for now
+		LUI R0,#160
+		LLI R0,#0
+		LDW R0,[R0,#0]	; R1 contains read serial data
+		ENAI
 		PUSH R1
 		PUSH R2
 		PUSH R3
-
-		; Read serial data
-		LUI R0,#160
-		LLI R0,#0
-		LDW R1,[R0,#0]	; R1 contains read serial data
-
+		PUSH R4
+		
 		; Get read ptr
-		LUI R0,#1
-		LLI R0,#0
-		LDW R2,[R0,#0]	; R2 contains read ptr
+		LUI R1,#1
+		LLI R1,#0
+		LDW R2,[R1,#0]	; R2 contains read ptr
 		
 		; Get write ptr
-		ADDI R3,R0,#1
-		LDW R2,[R3,#0]	; R4 contain the write ptr
-	
+		ADDI R3,R1,#1
+		LDW R4,[R3,#0]	; R4 contain the write ptr
+
+		; See how far behind the read ptr is
+			
+
+
 		; Put serial data at ptr address
-		STW R1,[R2,#0]	; Write to buffer
-		ADDIB R2,#1
-		STW R2,[R3,#0]	; Inc write ptr
+		STW R0,[R4,#0]	; Write to buffer
+		ADDIB R4,#1
+		SUB R1,R1,R1
+		LUI R1,#1
+		LLI R1,#6
+		SUB R1,R1,R4
+		BE .wrapW
+		SUBIB R4,#4
+.wrapW	STW R4,[R3,#0]	; Inc write ptr
 	
+
+
+.isrOut	POP R4
 		POP R3
 		POP R2
 		POP R1
 		POP R0
 		RETI
-		
+	
+
+
 .main	LUI R0, #1		; Read ptr address in R0
 		LLI R0, #0	
 		LDW R2,[R0,#0]	; Read ptr in R2
@@ -63,8 +79,16 @@
 		POP R3
 		LUI R4,#8	
 		LLI R4,#1		; Address of LEDs
-		STW R3,[R4,#0]	; Put read data on LEDs
+		STW R3,[R4,#0]	; Put factorial on LEDs
 		ADDIB R2,#1		; Inc read ptr
+		SUB R0,R0,R0
+		LUI R0,#1
+		LLI R0,#6
+		SUB R0,R0,R2
+		BE .wrapR
+		SUBIB R2,#4
+.wrapR	LUI R0, #1		; Read ptr address in R0
+		LLI R0, #0	
 		STW R2,[R0,#0]	; Store new read pointer
 		BR .main       	; look again	
 
