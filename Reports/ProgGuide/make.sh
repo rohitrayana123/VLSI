0d7600f2b20ab628dc409018f224ac18ea63e93d
seblovett <seblovett@googlemail.com>
2014-04-13 21:34:06 +0100

    Make file added

---
+++ Reports/ProgGuide/make.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# @file make.sh
+# Date Created: Sun 13 Apr 2014 20:22:37 BST by seblovett on seblovett-Ubuntu
+# <+Last Edited: Sun 13 Apr 2014 20:22:52 BST by seblovett on seblovett-Ubuntu +>
+
+pdflatex ProgGuide.tex
+pdflatex ProgGuide.tex
+pdflatex ProgGuide.tex
