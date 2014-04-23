afc0ef5baee94367540f507227a6442e18fa268c
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-04-23 02:04:05 +0100

    code

--- Reports/ProgGuide/pseudocode/shiftAndAdd.c
+++ Reports/ProgGuide/pseudocode/shiftAndAdd.c
@@ -1,13 +1,18 @@
-unsigned short multi(unsigned char mul, unsigned char quo){
-	unsigned char M,Q,i;
-	unsigned short A;
+uint16_t multi(uint16_t mul, quo){
+	uint16_t A,M,Q,i;
+	if((mul && 0xFF00)||(quo && 0xFF00)){
+		return 0;
+	}
 	A = 0; M = mul; Q = quo;
-	for(i=0;i<16;i++){
-		if(M[0] == 1){
+	for(i=0;i<7;i++){
+		if(M && 0x0001){ 
 			A = A + Q;
 		}
-		Q = Q << 1;
+		Q = Q << 1; 
 		M = M >> 1;
+	}
+	if(M && 0x0001){ 
+		A = A + Q;
 	}
 	return A;
 }
