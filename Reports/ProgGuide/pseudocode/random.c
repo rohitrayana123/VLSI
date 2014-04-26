1a03c7f749c573fc9736a88e6e4148513145d772
Ashley Robinson <ashley181291@gmail.com>
2014-04-26 19:33:48 +0100

    more words

--- Reports/ProgGuide/pseudocode/random.c
+++ Reports/ProgGuide/pseudocode/random.c
@@ -1,9 +1,10 @@
 uint16_t rand(uint16_t last){
 	uint16_t next,test;
-	next = last >> 1;						// Shift reg
-	test = last & 0x000A;					// Bits 4 and 1
-	if((test == 0x0000)|(test == 0x000A)){	// XOR test
-		return next;					
-	}
-	return next | 0x8000;					// Feedback to top
+	next = last >> 1;				// The shift
+	test = last << 2;				// Compare different bits
+	test = test ^ last;				
+	if(test & 0x0008){				// Feedback to top
+		return (next | 0x8000);
+	}	
+	return next;	
 }
