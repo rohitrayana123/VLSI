27da59995a9e6ebae44950cc0b3d4dcc85ef903b
seblovett <seblovett@googlemail.com>
2014-04-28 20:16:39 +0100

    Automated the addition of the help prompts. #92
    Any changes to the help won't need updating in the report

--- Reports/ProgGuide/make.sh
+++ Reports/ProgGuide/make.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # @file make.sh
 # Date Created: Sun 13 Apr 2014 20:22:37 BST by seblovett on seblovett-Ubuntu
-# <+Last Edited: Sun 13 Apr 2014 20:22:52 BST by seblovett on seblovett-Ubuntu +>
-
+# <+Last Edited: Mon 28 Apr 2014 20:16:01 BST by seblovett on seblovett-Ubuntu +>
+./../../bin/sim.py -h > simpy.txt
+./../../bin/assemble.py -h > asmhelp.txt
 pdflatex ProgGuide.tex
 pdflatex ProgGuide.tex
 pdflatex ProgGuide.tex
