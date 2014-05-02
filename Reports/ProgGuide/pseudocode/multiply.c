1488f6b92db3de833ab7b6107988dc3213908c1b
seblovett <seblovett@googlemail.com>
2014-05-02 11:24:15 +0100

    Removed parts about quick exit #92

--- Reports/ProgGuide/pseudocode/multiply.c
+++ Reports/ProgGuide/pseudocode/multiply.c
@@ -1,25 +1,12 @@
 uint16_t multi(uint16_t op1, op2){
 	uint16_t A,M,Q;	
 	A = 0; 
-	if(op1 < op2){				// Make M small, less loops
-		M = op1; Q = op2;	
-	}else{
-		M = op2; Q = op1;
-	}
 	while(1){					// No loop counter
-		if(M & 0x0001){ 		// LSb
+		if(M & 0x0001)		// LSb
 			A = A + Q;
-			if(A > 0xFFFF){		// Using carry flag
-				return 0;		// Overflow - fail
-			}
-		}
 		M = M >> 1;
-		if(0 == M){
+		if(0 == M)
 			return A;			// Finished - pass
-		}
-		if(Q & 0x8000){
-			return 0;			// Q >= 2^16 - fail
-		}
 		Q = Q << 1; 
 	}
 }
